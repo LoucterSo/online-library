cefebc912777d4d239d0ab5ac08358bb5734666f
LoucterSo <vlad_gorelin@inbox.ru>
2025-04-07 16:02:01 +0300

    Getting started

---
+++ src/test/java/io/github/LoucterSo/online_library/OnlineLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.LoucterSo.online_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
